cc60f4da443debbb173d2a9ebb39ede7b50f1587
Nayan Pahuja <nayanpahuja25@gmail.com>
2023-07-29 13:48:55 +0530

    'Added Landing Page UI'

--- app/src/main/java/com/pahuja/eduindia/screens/landingScreen.kt
+++ app/src/main/java/com/pahuja/eduindia/screens/landingScreen.kt
@@ -1,13 +1,13 @@
 package com.pahuja.eduindia.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,6 +16,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,8 +25,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.pahuja.eduindia.R
 
+//val customFont = FontFamily(
+//    fonts = listOf(
+//        Font(
+//            resId = R.font.mclaren,
+//            weight = FontWeight.Normal,
+//        )
+//    )
+//)
 @Composable
-
 fun LandingScreen(){
     //contains the surface for landingScreen
     Surface(
@@ -44,7 +53,8 @@ fun LandingScreen(){
                 color = Color.Black,
                 textAlign = TextAlign.Center,
                 style = TextStyle(
-                    fontSize = 18.sp
+                    fontSize = 18.sp,
+//                    fontFamily = customFont
 
                 ),
                 modifier = Modifier
@@ -91,8 +101,9 @@ fun LandingScreen(){
                 color = Color.Black,
                 textAlign = TextAlign.Center,
                 style = TextStyle(
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+//                    fontFamily = customFont
 
                 ),
                 modifier = Modifier
@@ -101,9 +112,24 @@ fun LandingScreen(){
                     .width(width = 109.dp)
                     .height(height = 36.dp)
             )
-            Button(onClick = { /*TODO*/ }) {
+            Button(
 
-            }
+                modifier = Modifier
+                    .width(width = 175.dp)
+                    .height(height = 45.dp)
+                    .offset(x = 107.dp, y = 690.dp),
+                onClick = { /*TODO*/ },
+                content = {
+                    Text(text = "Get Started!", fontSize = 16.sp, color = Color.White)
+                },
+                shape = RoundedCornerShape(20.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF191A37)),
+                border = BorderStroke(1.dp, Color.Black)
+
+
+            )
+
+
 
         }
         }
