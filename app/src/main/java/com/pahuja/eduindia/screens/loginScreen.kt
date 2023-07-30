ce9584f8e30b690951d2b528760fc360333d09ae
Nayan Pahuja <nayanpahuja25@gmail.com>
2023-07-30 18:37:36 +0530

    'Updated  Login Screen UI and and added components'

--- app/src/main/java/com/pahuja/eduindia/screens/loginScreen.kt
+++ app/src/main/java/com/pahuja/eduindia/screens/loginScreen.kt
@@ -1,12 +1,14 @@
 package com.pahuja.eduindia.screens
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,7 +24,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-
+val customFont = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = com.pahuja.eduindia.R.font.poppins_semi_bold
+        )
+    )
+)
 
 @Composable
 fun LoginScreen(){
@@ -94,6 +102,23 @@ fun LoginText(modifier: Modifier = Modifier){
 
 
     )
+}
+
+@Composable
+fun textFields(labelValue:String){
+    val textValue = remember{
+        mutableStateOf("")
+    }
+
+//    OutlinedTextField(
+//        label = Text(text = labelValue),
+//        value = textValue ,
+//        onValueChange = {
+//            textValue.value = it
+//        }
+
+
+
 }
 
 @Preview
